85e18e74b1917ebbbb134c37e00c5f075520c522
scolear <bobtera@gmail.com>
2018-03-30 18:03:39 +0200

    Refractoring again

--- src/functions.py
+++ src/functions.py
@@ -10,8 +10,10 @@ G = 1.487856e-34	# [AU^3*d^-2*kg^-1]
 
 class Planet:
 	""" An object with name, mass, pos, vel, acc, frc.
-	The force(self, other) function calculates newtonian gravitational force applied upon this object by the 'other'.
+	The force(self, other) function calculates
+	newtonian gravitational force applied upon this object by the 'other'.
 	"""
+	
 	def __init__(self, name, mass):
 		self.name = name
 		self.mass = mass
@@ -47,7 +49,8 @@ def SolarSystem_init(init_file_path, inbb):
 		sel_pl = df_start.iloc[i]
 		name = sel_pl['Name']
 		
-		if not inbb:									# Skip inner planets, if inbb flag is False
+		# Skip inner planets, if inbb flag is False:
+		if not inbb:
 			if name in ['Venus', 'Earth', 'Mars']:
 				continue
 		
@@ -67,28 +70,33 @@ def Acceleration(N, planets):
 	""" Calculates the applied resultant force on 'N' 'planets' by all the others.
 	Then calculates acceleration as well.
 	"""
-	Fmat = [[[0.0] for x in range(N)] for x in range(N)]	# Redrawing the forcematrix every time
+	
+	# Redrawing the forcematrix every time:
+	Fmat = [[[0.0] for x in range(N)] for x in range(N)]
+	
 	for i in range(N):
 		j = i+1
-		planets[i].frc = np.array([0.,0.,0.])				# Emptying every time
+		planets[i].frc = np.array([0.,0.,0.])				
 	
+		# Forcematrix:
 		while j < N:
-			Fmat[i][j] = planets[i].force(planets[j])		# Forcematrix
+			Fmat[i][j] = planets[i].force(planets[j])
 			Fmat[j][i] = (-1) * Fmat[i][j]
 			j += 1
 		
+		# Resultant force:
 		for k in range(N):
-			planets[i].frc += Fmat[i][k]					# Resultant force
+			planets[i].frc += Fmat[i][k]					
 		
-		#print(planets[i].name, planets[i].frc) 
-		
-		planets[i].acc = planets[i].frc / planets[i].mass	# Calculating acceleration
+		# Calculating acceleration:
+		planets[i].acc = planets[i].frc / planets[i].mass
 
 
 def Euler(N, pl, dt):
 	""" One round of Euler integration for N planets, using a fixed dt timestep.
 	Putting the acceleration update between the pos and vel updates creates the Euler-Cromer method,
-	which is a much more stable alternative."""
+	which is a much more stable alternative.
+	"""
 	
 	Acceleration(N, pl)									# acceleration update
 	
@@ -98,19 +106,20 @@ def Euler(N, pl, dt):
 
 
 def Verlet(n, pl, dt):
-	""" Does one round of Verlet integration from initial r, v, a of 'n' number of planets, using 'pl' as a list of planet objects, and using a 'dt' timestep.
+	""" Does one round of Verlet integration from initial r, v, a of 'n' number of planets,
+	using 'pl' as a list of planet objects, and using a 'dt' timestep.
 	"""
 	
-	#update x using acc(x) for all planets
+	# Update x using acc(t) for all planets:
 	for i in range(n):
 		pl[i].pos = pl[i].pos + pl[i].vel * dt + 0.5 * pl[i].acc * dt**2
-		#store acc(x) in temp
+		# Store acc(t) in temp:
 		pl[i].acc_temp = pl[i].acc
 		
-	#calc acc(x+1)
+	# Calculate acc(t+1):
 	Acceleration(n, pl)
 	
-	#calc v(x+1) using temp+acc(x+1)
+	# Calculate v(t+1) using temp + acc(t+1):
 	for i in range(n):
 		pl[i].vel = pl[i].vel + 0.5 * (pl[i].acc_temp + pl[i].acc) * dt
 
@@ -133,26 +142,24 @@ def RK4(u, dudt, n, t, h, derivs, planets):
 	b3 = (1.0 / 3.0)
 	b4 = (1.0 / 6.0)
 	
-	K1, K2, K3, K4 = [np.array([]) for _ in range(4)]	
-	K1 = [np.array([]) for _ in range(n)]								# So it is indexable
+	K2, K3, K4 = [np.array([]) for _ in range(3)]	
 	utemp = [np.array([]) for _ in range(n)]
 	unew = [np.array([]) for _ in range(n)]
 	
 	for i in range(n):					  # First step
-		K1[i] = dudt[i]					  # Just for brevity
-		utemp[i] = u[i] + h*a21*K1[i]
+		utemp[i] = u[i] + h*a21*dudt[i]
 	K2 = derivs(t + c2*h, utemp, planets)	# Second step; Array!
 	
 	for i in range(n):						
-		utemp[i] = u[i] + h*(a31*K1[i] + a32*K2[i])
+		utemp[i] = u[i] + h*(a31*dudt[i] + a32*K2[i])
 	K3 = derivs(t + c3*h, utemp, planets)	# Third step
 	
 	for i in range(n):
-		utemp[i] = u[i] + h*(a41*K1[i] + a42*K2[i] + a43*K3[i])
+		utemp[i] = u[i] + h*(a41*dudt[i] + a42*K2[i] + a43*K3[i])
 	K4 = derivs(t + c4*h, utemp, planets)	# Fourth step
 	
 	for i in range(n):						
-		unew[i] = u[i] + h*(b1*K1[i] + b2*K2[i] + b3*K3[i] + b4*K4[i])			# Accumulate increments
+		unew[i] = u[i] + h*(b1*dudt[i] + b2*K2[i] + b3*K3[i] + b4*K4[i])			# Accumulate increments
 		
 	return unew
 
@@ -165,7 +172,10 @@ def Reset(planets, T, step):
 	
 	
 def total_energy(N, planets):
-	""" Calculates total energy of the system of N planets by calculating Kinetic energy K, and Potential energy U."""
+	""" Calculates total energy of the system of N planets
+	by calculating Kinetic energy K, and Potential energy U.
+	"""
+	
 	K = 0
 	U = 0
 	
@@ -181,7 +191,9 @@ def total_energy(N, planets):
 
 
 def log_data(file, step, T, N, planets):
-	""" This function logs the current time T, the total energy of the system, and the x,y,z coordinates of all the objects."""
+	""" This function logs the current time T, the total energy of the system,
+	and the x,y,z coordinates of all the objects.
+	"""
 
 	Etot = total_energy(N, planets)
 	
@@ -201,15 +213,23 @@ def log_data(file, step, T, N, planets):
 
 				
 def clear_logs():
+	""" A function that prompts the user whether to clear library of previous logs.
+	Returns 
+	1 if the answer is yes,
+	0 if the answer is no.
+	"""
+	
 	del_choice = input('Do you want to clear library of previous logs? [y/n]: ')
+	
 	if del_choice == 'y' or del_choice == 'yes':
-		for path in iglob('.' + sep + 'logs' + sep + 'output*'):		# Clearing previous runs
+		for path in iglob('.' + sep + 'logs' + sep + 'output*'):
 			os.remove(path)
 		os.remove('.' + sep + 'logs' + sep + 'CPUlogs.csv')
 		for path in iglob('.' + sep + 'logs' + sep + 'RKDP_ERRS_*'):
 			os.remove(path)
 		print("Library cleared.\n")
 		return 1
+		
 	elif del_choice == 'n' or del_choice == 'no':
 		print('Leaving previous logs intact.\n')
 		return 0
