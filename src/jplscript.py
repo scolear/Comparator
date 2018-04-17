a660f13574b3c4fb68f8f805795d782980786855
scolear <bobtera@gmail.com>
2018-04-17 15:19:40 +0200

    up

--- src/jplscript.py
+++ src/jplscript.py
@@ -7,7 +7,10 @@ JPL_CG= []
 
 def main():
 	for ts in range(w_ts_range.result[0], w_ts_range.result[1]):
-	
+		
+		# since epoch only takes a dictionary of the following form, we could work around it by creating a list of JDs, and then passing them as discrete times. These should be profiled....
+		
+		
 		Jup = Horizons(id = '599', id_type = 'id', epochs = {'start': '1900-01-01 00:00', 'stop': , 'step': (str(ts)+'d')})
 		vec = Jup.vectors()
 		
