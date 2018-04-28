079c0fbde6a72258ebb4bb3ad8ecd08a78198db7
scolear <bobtera@gmail.com>
2018-04-28 14:37:49 +0200

    jpl_query_encapsulation

--- src/bokehorbits.py
+++ src/bokehorbits.py
@@ -1,5 +1,8 @@
 from glob import iglob
 from os import sep
+import math
+
+from astroquery.jplhorizons import Horizons
 
 import pandas as pd
 import numpy as np
@@ -10,8 +13,78 @@ from bokeh.models.widgets import Panel, Tabs
 from bokeh.models import ColumnDataSource, HoverTool
 output_notebook()
 
+def jd_to_date(jd):
+    """
+    Convert Julian Day to date.
+	:Author: Matt Davis
+    
+    Algorithm from 'Practical Astronomy with your Calculator or Spreadsheet', 
+        4th ed., Duffet-Smith and Zwart, 2011.
+    
+    Parameters
+    ----------
+    jd : float
+        Julian Day
+        
+    Returns
+    -------
+    year : int
+        Year as integer. Years preceding 1 A.D. should be 0 or negative.
+        The year before 1 A.D. is 0, 10 B.C. is year -9.
+        
+    month : int
+        Month as integer, Jan = 1, Feb. = 2, etc.
+    
+    day : float
+        Day, may contain fractional part.
+        
+    Examples
+    --------
+    Convert Julian Day 2446113.75 to year, month, and day.
+    
+    >>> jd_to_date(2446113.75)
+    (1985, 2, 17.25)
+    
+    """
+    jd = jd + 0.5
+    
+    F, I = math.modf(jd)
+    I = int(I)
+    
+    A = math.trunc((I - 1867216.25)/36524.25)
+    
+    if I > 2299160:
+        B = I + 1 + A - math.trunc(A / 4.)
+    else:
+        B = I
+        
+    C = B + 1524
+    
+    D = math.trunc((C - 122.1) / 365.25)
+    
+    E = math.trunc(365.25 * D)
+    
+    G = math.trunc((C - E) / 30.6001)
+    
+    day = C - E + F - math.trunc(30.6001 * G)
+    
+    if G < 13.5:
+        month = G - 1
+    else:
+        month = G - 13
+        
+    if month > 2.5:
+        year = D - 4716
+    else:
+        year = D - 4715
+        
+    return year, month, day
+
+	
 def page_setup(pages, df_JPL_CG, df_JPL_JUP, hover):
-	# Plotting JPL data for C-G on every figure and setting up styling
+	"""
+	Plotting JPL data for C-G and Jupiter on every figure and setting up styling.
+	"""
 	sourceCG = ColumnDataSource(df_JPL_CG)
 	sourceJUP = ColumnDataSource(df_JPL_JUP)
 
@@ -23,6 +96,39 @@ def page_setup(pages, df_JPL_CG, df_JPL_JUP, hover):
 		page.xaxis.axis_label = "X [AU]"
 		
 
+def JPL_query(Ttot, ts):
+	"""
+	Querying JPL Horizons system for the state-vectors of Jupiter and 67P/C-G,
+	then converting the tables to pandas dataframes.
+	"""
+	JD_period_end = 2415020.5 + Ttot
+	
+	if JD_period_end > 2667387.5:
+		print('Timescale is too large for query. Using previously downloaded HORIZONS data.')
+		df1 = pd.read_csv('.' + sep + 'addendum' + sep + 'Jupiter_1900_2000_1.csv', skipinitialspace = True, float_precision = 'high')
+		df2 = pd.read_csv('.' + sep + 'addendum' + sep + '67P_1900_2000_1.csv', skipinitialspace = True, float_precision = 'high')
+		
+	else:
+		endy, endm, endd = jd_to_date(JD_period_end)
+		end = str(endy) + '-' + str(endm) + '-' + str(endd)
+	
+		print(f'Querying HORIZONS with a {ts} days timestep...')
+	
+		# Jupiter data:
+		Jup = Horizons(id = '599', id_type = 'id', location = '500@0', epochs = {'start': '1900-01-01', 'stop': end, 'step': (str(ts)+'d')})
+		vec = Jup.vectors()
+	
+		df1 = vec.to_pandas()
+	
+		# 67P/C-G data:
+		CG = Horizons(id = '900681', id_type = 'id', location = '500@0', epochs = {'start': '1900-01-01', 'stop': end, 'step': (str(ts)+'d')})
+		vec2 = CG.vectors()
+	
+		df2 = vec2.to_pandas()
+	
+	return df1, df2
+	
+
 def main():
 
 	if w_cg.result is True:
@@ -32,16 +138,14 @@ def main():
 		name_list = ["Jupiter", "Saturn", "Uranus", "Neptune"]
 		name = "Jupiter"
 	
+	Ttot = w_Ttot.result
 	ts_range_max = w_ts_range.result[1]
 	ts_range_min = w_ts_range.result[0]
 	tol_range_max = w_err_range.result[1]
 	tol_range_min = w_err_range.result[0]	
-
-	# df_JPL_JUP = pd.read_csv('.' + sep + 'addendum' + sep + 'Jupiter_1900_2000_1.csv', skipinitialspace = True, float_precision = 'high')
-	# df_JPL_CG = pd.read_csv('.' + sep + 'addendum' + sep + '67P_1900_2000_1.csv', skipinitialspace = True, float_precision = 'high')
 	
-	df_JPL_JUP = JPL_JUP[0]
-	df_JPL_CG = JPL_CG[0]
+	# If timescale allows, query the HORIZONS system for accurate data, using the same stepsize:
+	df_JPL_JUP, df_JPL_CG = JPL_query(Ttot, ts_range_min)
 	
 	# Creating T column of JPL dataframes:
 	for df in [df_JPL_JUP, df_JPL_CG]:
@@ -124,12 +228,12 @@ def main():
 			
 	page1.legend.location = "bottom_left"
 	
+	# The figure in the end consists of separate tabs, each containing one 'page':
 	tablist = []
 	z = 0
-	for page in pages:
 	
+	for page in pages:
 		page.legend.click_policy = "hide"
-		
 		title = ['All', 'Euler', 'Verlet', 'RK4', 'RKDP']
 		tab = Panel(child = page, title = title[z])
 		tablist.append(tab)
